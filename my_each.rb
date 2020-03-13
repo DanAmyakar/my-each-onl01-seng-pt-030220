5572a6f4e7cb9267186c6ab0251feef72b04d7b7
Daniel Amyakar <dan.rockit@gmail.com>
2020-03-13 01:35:49 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,11 +1,11 @@
-def my_each(aar_1) # put argument(s) here
+def my_each(arr_1) # put argument(s) here
   
   # code here
    if block_given?
     i = 0
     while i < arr_1.length
-      
-      yield (arr_1[i])
+       
+      yield arr_1[i]
       i += 1
     end
     arr_1
